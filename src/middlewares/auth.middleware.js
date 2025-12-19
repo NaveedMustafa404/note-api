9938b241ed3cec60d9af8ecffee51676fa96803e
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2025-12-19 19:34:20 +0600

    feat(auth): add JWT authentication middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,16 @@
+import {verifyToken} from "../utils/jwt.js";
+
+export const isAuthenticated = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {    
+            return res.status(401).json({ success : false, message : "Unauthorized" });
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded = verifyToken(token);
+        req.user = { id: decoded.id, email: decoded.email }; 
+        next();
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Invalid or token expired" });
+    }
+};
