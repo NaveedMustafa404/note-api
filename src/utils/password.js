0701b9104b1b3a0d6c1c4e459ed359874b396918
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2025-12-19 19:17:09 +0600

    feat(auth): add password and jwt utility functions

---
+++ src/utils/password.js
@@ -0,0 +1,11 @@
+
+import bcrypt from "bcrypt";
+
+
+export const hashPassword = async (password) => {
+  return bcrypt.hash(password, 12);
+};
+
+export const verifyPassword = async (password, hashPassword) => {
+  return bcrypt.compare(password, hashPassword);
+};
