6d5aefb1e84ce234518c31bdb289b67004aeb64f
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2025-12-19 10:01:21 +0600

    feat(databse): add Sequalize singleton with connection pooling

---
+++ src/config/database.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import { initModels } from "../models/index.js";
+
+let sequelize;
+
+export const connectDB = async () => {
+    if (!sequelize) {
+        sequelize = new Sequelize(
+            process.env.DB_NAME,
+            process.env.DB_USER,
+            process.env.DB_PASSWORD,
+            {
+                host: process.env.DB_HOST,
+                dialect: "mysql",
+                logging : false,
+                pool: {
+                    max: 10,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000,
+                },
+            }
+        );
+        initModels(sequelize);
+        
+    }
+    return sequelize;
+            };
